284a5ed307e7bf789a44e5cac957451caac2ed6c
HectorGarcia01 <hg2779948@gmail.com>
2023-07-21 19:23:31 -0600

    Configuración de cors para aceptar solicitudes con los métodos GET, POST, PATCH y DELETE además de los encabezados para permitir la autenticación, etc

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ const express = require('express');                                    //Cargamo
 
 const app = express();                                                 //Creamos una instancia de la aplicación de express
 
+//Configuración de cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization");
+    next();
+});
+
 //Configuramos un manejo de rutas inexistentes
 app.get('*', (req, res) => {
     //Enviamos un estado de error 404 y un objeto
