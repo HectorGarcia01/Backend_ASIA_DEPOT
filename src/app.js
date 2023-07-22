2713c1b99d82b52d657378b7dfccbd7ef81fabc1
HectorGarcia01 <hg2779948@gmail.com>
2023-07-21 19:21:18 -0600

    Configuración del manejo de rutas inexistentes a la hora de usar el método get

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ const express = require('express');                                    //Cargamo
 
 const app = express();                                                 //Creamos una instancia de la aplicación de express
 
+//Configuramos un manejo de rutas inexistentes
+app.get('*', (req, res) => {
+    //Enviamos un estado de error 404 y un objeto
+    res.status(404).send({
+        title: '404',
+        errorMessage: "Página no encontrada."
+    });
+});
+
 //Iniciamos el servidor 
 app.listen(PORT, () => {
     console.log(`Servidor inicializado en el puerto: ${ PORT }`);
