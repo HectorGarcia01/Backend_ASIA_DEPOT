fd8cf03e1654ba122fc11601a3574836aeee6ddd
HectorGarcia01 <hg2779948@gmail.com>
2023-08-03 03:18:14 -0600

    Creación del modelo Direccion

---
+++ src/models/address.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db_connection');
+
+/**
+ * Creación del modelo Direccion
+ * Fecha creación: 03/08/2023
+ * Autor: Hector Armando García González
+ */
+
+const Direccion = db.define('Direccion', {
+    Departamento: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    Municipio: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    Calle: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+});
+
+//Exportación del modelo Direccion
+module.exports = Direccion;
