7d838c773efff41792c1164ff281f2c64d32c616
HectorGarcia01 <hg2779948@gmail.com>
2023-08-03 02:55:34 -0600

    Creación del modelo Cliente con sus respectivos atributos, tipo de datos y llaves foráneas

---
+++ src/models/customer.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db_connection');
+
+/**
+ * Creación del modelo Cliente
+ * Fecha creación: 03/08/2023
+ * Autor: Hector Armando García González
+ */
+
+const Cliente = db.define('Cliente', {
+    Nombre_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Apellido_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Telefono_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    NIT_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    Correo_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        low: true,
+        unique: true
+    },
+    Password_Cliente: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Avatar_Cliente: {
+        type: DataTypes.BLOB,
+        allowNull: true
+    },
+    ID_Estado_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Estado',
+            key: 'id'
+        }
+    },
+    ID_Direccion_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Direccion',
+            key: 'id'
+        }
+    }
+});
+
+//Exportación del modelo Cliente
+module.exports = Cliente
