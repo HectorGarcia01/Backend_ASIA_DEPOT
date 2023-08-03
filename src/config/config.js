859465229f1b4ebe82736929014b89f6e3dc6547
HectorGarcia01 <hg2779948@gmail.com>
2023-08-03 01:59:41 -0600

    Agregando las variables de entorno para la configuración de la db

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,9 @@ require('dotenv').config({ path: `.dev.env` });
 
 //Exportando las variables de entorno
 module.exports = {
-    PORT: process.env.PORT
+    PORT: process.env.PORT,
+    DATABASE: process.env.DATABASE,
+    USERNAME_DB: process.env.USERNAME_DB,
+    PASSWORD_DB: process.env.PASSWORD_DB,
+    HOST_DB: process.env.HOST_DB
 }
