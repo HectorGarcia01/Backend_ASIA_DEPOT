19e3778b8437cfdf6f609c2bbabedbd14ea568a7
HectorGarcia01 <hg2779948@gmail.com>
2023-08-03 03:56:15 -0600

    Creación del modelo Token

---
+++ src/models/token.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db_connection');
+
+/**
+ * Creación del modelo Token
+ * Fecha creación: 03/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: Modelo Cliente (customer.js) y Modelo Empleado (employee.js)
+ */
+
+const Token = db.define('Token', {
+    Token_Usuario: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Tipo_Usuario: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        values: ['Cliente', 'Empleado']
+    },
+    ID_Cliente_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: 'Cliente',
+            key: 'id'
+        }
+    },
+    ID_Empleado_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: 'Empleado',
+            key: 'id'
+        }
+    }
+});
+
+//Exportación del modelo Token
+module.exports = Token;
