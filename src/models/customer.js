42f99a0b7babfec3278db4fdb299dfd94f1dea25
HectorGarcia01 <hg2779948@gmail.com>
2023-08-04 04:46:07 -0600

    Creación de un método personalizado en el modelo Cliente para la generación de tokens de autenticación

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,6 +1,8 @@
 const { DataTypes } = require('sequelize');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const db = require('../database/db_connection');
+const { KEY_TOKEN } = require('../config/config');
 
 /**
  * Creación del modelo Cliente
@@ -52,7 +54,7 @@ const Cliente = db.define('Cliente', {
 
 /**
  * Hook para el cifrado de contraseña
- * Fecha creación: 03/08/2023
+ * Fecha creación: 04/08/2023
  * Autor: Hector Armando García González
  * Referencias: Datos del cliente actual
  */
@@ -61,9 +63,26 @@ Cliente.beforeCreate(async (cliente) => {
     try {
         cliente.Password_Cliente = await bcrypt.hash(cliente.Password_Cliente, 8);
     } catch (error) {
-        throw new Error(error);
+        throw new Error({ mensajeError: "Error al cifrar la contraseña.", error });
     }
-})
+});
+
+/**
+ * Método personalizado para generar tokens
+ * Fecha creación: 04/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: Variable de entorno para llave secreta (config.js)
+ */
+
+Cliente.prototype.generarToken = async function () {
+    try {
+        const cliente = this;
+        const token = jwt.sign({ id: cliente.id.toString() }, KEY_TOKEN);
+        return token;
+    } catch (error) {
+        throw new Error({ mensajeError: "Error al generar token.", error });
+    }
+}
 
 //Exportación del modelo Cliente
 module.exports = Cliente;
