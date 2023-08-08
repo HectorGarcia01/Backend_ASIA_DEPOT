a066ffc2f85041bcbfa424c6b9b47de5667d6730
HectorGarcia01 <hg2779948@gmail.com>
2023-08-07 19:17:00 -0600

    Implementación del middleware de validación de datos de entrada con el esquema de validación de cliente como parámetro a la ruta para crear un nuevo cliente

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -3,15 +3,20 @@ const router = new express.Router();
 const {
     crearCliente
 } = require('../controllers/customer.controller');
+const esquemaValidacion = require('../schemas/customer.schema');
+const middlewareValidate = require('../middlewares/validate');
 
 /**
  * Creación de rutas (endpoints) para clientes
  * Fecha creación: 03/08/2023
  * Autor: Hector Armando García González
  * Referencias: 
- *              Controlador cliente (customer.controller.js)
+ *              Controlador cliente (customer.controller.js),
+ *              Esquema de validación de cliente (customer.schema.js),
+ *              Middleware de validación (validate.js)
  */
-router.post('/nuevo/cliente', crearCliente);
+
+router.post('/nuevo/cliente', middlewareValidate(esquemaValidacion), crearCliente);
 
 //Exportación de todas las rutas de cliente
 module.exports = router;
