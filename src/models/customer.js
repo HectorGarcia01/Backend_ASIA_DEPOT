68a463b3a613519e0f71a26249fb15eca7c26779
HectorGarcia01 <hg2779948@gmail.com>
2023-08-09 01:09:50 -0600

    Modificación del modelo Cliente, se le agregó una llave foránea para el rol

--- src/models/customer.js
+++ src/models/customer.js
@@ -8,7 +8,9 @@ const { KEY_TOKEN } = require('../config/config');
  * Creación del modelo Cliente
  * Fecha creación: 03/08/2023
  * Autor: Hector Armando García González
- * Referencias: Modelo Estado (state.js)
+ * Referencias: 
+ *              Modelo Estado (state.js).
+ *              Modelo Rol (role.js).
  */
 
 const Cliente = db.define('Cliente', {
@@ -47,6 +49,14 @@ const Cliente = db.define('Cliente', {
         allowNull: false,
         references: {
             model: 'Estados',
+            key: 'id'
+        }
+    },
+    ID_Rol_FK: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'Rols',
             key: 'id'
         }
     }
