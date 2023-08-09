073b1f90a99c8b70041e878bd9d48c4dea05433c
HectorGarcia01 <hg2779948@gmail.com>
2023-08-09 01:06:13 -0600

    Modificación del comentario

--- src/models/role.js
+++ src/models/role.js
@@ -5,7 +5,6 @@ const db = require('../database/db_connection');
  * Creación del modelo Rol
  * Fecha creación: 03/08/2023
  * Autor: Hector Armando García González
- * Referencias: Modelo Cliente (customer.js) y Modelo Empleado (employee.js)
  */
 
 const Rol = db.define('Rol', {
