9f7b068342ae787b99734fdc86a91658b8391117
HectorGarcia01 <hg2779948@gmail.com>
2023-08-11 17:48:37 -0600

    Creación del middleware de autenticación

---
+++ src/middlewares/auth.js
@@ -0,0 +1,62 @@
+const jwt = require('jsonwebtoken');
+const { KEY_TOKEN } = require('../config/config');
+const Cliente = require('../models/customer');
+const Empleado = require('../models/employee');
+const Token = require('../models/token');
+
+/**
+ * Middleware de autenticación
+ * Fecha creación: 05/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Cliente (customer.js),
+ *              Modelo Empleado (employee.js),
+ *              Modelo Token (token.js)
+ */
+
+const autenticación = (rol) => async (req, res, next) => {
+    try {
+        if (!req.header('Authorization')) {
+            throw new Error("¡No se proporcionó un token!");
+        }
+
+        const Token_Usuario = req.header('Authorization').replace('Bearer ', '');
+        const decodificarToken = jwt.verify(Token_Usuario, KEY_TOKEN);
+        const { id } = Token.findOne({
+            where: {
+                Token_Usuario
+            }
+        });
+
+        let usuario;
+
+        if (rol === 'User') {
+            usuario = await Cliente.findOne({
+                where: {
+                    id: decodificarToken.id,
+                    ID_Rol_FK: id
+                }
+            });
+        } else {
+            usuario = await Empleado.findOne({
+                where: {
+                    id: decodificarToken.id,
+                    ID_Rol_FK: id
+                }
+            });
+        }
+
+        if (!usuario) {
+            throw new Error("El token es inválido.");
+        }
+
+        req.usuario = usuario;
+        req.token = Token_Usuario;
+
+        next();
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+};
+
+module.exports = autenticación;
