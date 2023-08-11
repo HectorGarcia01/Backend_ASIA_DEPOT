3bee218b2672531704cc30f480843f4f0b85d294
HectorGarcia01 <hg2779948@gmail.com>
2023-08-10 23:13:32 -0600

    Eliminación de comentarios

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -6,16 +6,6 @@ const {
 const esquemaValidacion = require('../schemas/employee.schema');
 const middlewareValidate = require('../middlewares/validate');
 
-/**
- * Creación de rutas (endpoints) para empleados
- * Fecha creación: 04/08/2023
- * Autor: Hector Armando García González
- * Referencias: 
- *              Controlador empleado (employee.controller.js)
- *              Esquema de validación de cliente (employee.schema.js),
- *              Middleware de validación (validate.js)
- */
-
 router.post('/superAdmin/nuevo/empleado', middlewareValidate(esquemaValidacion), crearEmpleado);
 
 //Exportación de todas las rutas de empleado
