e2865c4aebf25766816678fa14e5aadbe1092153
HectorGarcia01 <hg2779948@gmail.com>
2023-08-10 23:13:49 -0600

    Eliminación de comentarios

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -6,16 +6,6 @@ const {
 const esquemaValidacion = require('../schemas/customer.schema');
 const middlewareValidate = require('../middlewares/validate');
 
-/**
- * Creación de rutas (endpoints) para clientes
- * Fecha creación: 03/08/2023
- * Autor: Hector Armando García González
- * Referencias: 
- *              Controlador cliente (customer.controller.js),
- *              Esquema de validación de cliente (customer.schema.js),
- *              Middleware de validación (validate.js)
- */
-
 router.post('/nuevo/cliente', middlewareValidate(esquemaValidacion), crearCliente);
 
 //Exportación de todas las rutas de cliente
