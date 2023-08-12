f25d36df1cda240959c380381fd56b1e54c0e856
HectorGarcia01 <hg2779948@gmail.com>
2023-08-11 22:01:02 -0600

    Definiendo las rutas (endpoint) para cerrar la sesión o cerrar todas las sesiones

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    iniciarSesion
+    iniciarSesion,
+    cerrarSesion,
+    cerrarTodasSesiones
 } = require('../controllers/auth.controller');
 const esquemaValidacion = require('../schemas/auth.schema');
 const middlewareValidate = require('../middlewares/validate');
+const middlewareAuth =  require('../middlewares/auth');
 
 router.post('/usuario/login', middlewareValidate(esquemaValidacion), iniciarSesion);
+router.post('/usuario/logout', middlewareAuth, cerrarSesion);
+router.post('/usuario/logoutAll', middlewareAuth, cerrarTodasSesiones);
 
 //Exportación de todas las rutas de autenticación
 module.exports = router;
