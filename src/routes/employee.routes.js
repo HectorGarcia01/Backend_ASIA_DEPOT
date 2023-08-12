55d6b08425db011ec6b5900870a71358885e4468
HectorGarcia01 <hg2779948@gmail.com>
2023-08-11 22:16:29 -0600

    Implementación de la ruta (endpoint) para ver el pefil del empleado, se le aplicó el middleware de autenticación para validar que esté logueado

--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    crearEmpleado
+    crearEmpleado,
+    verPerfilEmpleado
 } = require('../controllers/employee.controller');
 const esquemaValidacion = require('../schemas/employee.schema');
 const middlewareValidate = require('../middlewares/validate');
+const middlewareAuth = require('../middlewares/auth');
 
 router.post('/superAdmin/nuevo/empleado', middlewareValidate(esquemaValidacion), crearEmpleado);
+router.get('/admin/ver/perfil', middlewareAuth, verPerfilEmpleado);
 
 //Exportación de todas las rutas de empleado
 module.exports = router;
