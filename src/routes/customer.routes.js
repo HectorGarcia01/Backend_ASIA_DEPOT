b7a1e0ebf5a1d3bef02e05bcb50cbc86aa9e676a
HectorGarcia01 <hg2779948@gmail.com>
2023-08-11 22:19:49 -0600

    Implementación de la ruta (endpoint) para ver el pefil del cliente, se le aplicó el middleware de autenticación para validar que esté logueado

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    crearCliente
+    crearCliente,
+    verPerfilCliente
 } = require('../controllers/customer.controller');
 const esquemaValidacion = require('../schemas/customer.schema');
 const middlewareValidate = require('../middlewares/validate');
+const middlewareAuth = require('../middlewares/auth');
 
 router.post('/nuevo/cliente', middlewareValidate(esquemaValidacion), crearCliente);
+router.get('/user/ver/perfil', middlewareAuth, verPerfilCliente);
 
 //Exportación de todas las rutas de cliente
 module.exports = router;
