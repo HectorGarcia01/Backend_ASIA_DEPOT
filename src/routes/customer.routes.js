fb91daf4567986e08eec97400a2f37e3b9f57db3
HectorGarcia01 <hg2779948@gmail.com>
2023-08-16 19:48:34 -0600

    Cambio de nombre de la ruta (endpoint) para ver el perfil del cliente

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -9,7 +9,7 @@ const middlewareValidate = require('../middlewares/validate');
 const middlewareAuth = require('../middlewares/auth');
 
 router.post('/nuevo/cliente', middlewareValidate(esquemaValidacion), crearCliente);
-router.get('/user/ver/perfil', middlewareAuth, verPerfilCliente);
+router.get('/usuario/ver/perfil', middlewareAuth, verPerfilCliente);
 
 //Exportación de todas las rutas de cliente
 module.exports = router;
