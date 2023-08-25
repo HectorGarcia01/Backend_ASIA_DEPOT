e38ff5de5f99b23692515b12265035cc50498893
HectorGarcia01 <hg2779948@gmail.com>
2023-08-25 04:28:22 -0600

    Configuración de las rutas (endpoints) para obtener todos los proveedores de la bd

--- src/routes/supplier.routes.js
+++ src/routes/supplier.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    addSupplier
+    addSupplier,
+    readSuppliers
 } = require('../controllers/supplier.controller');
 const supplierSchema = require('../schemas/supplier.schema');
 const validateMiddleware = require('../middlewares/validate');
@@ -10,21 +11,23 @@ const roleMiddleware = require('../middlewares/check_rol');
 
 //Configuración de rutas (endpoints) para el SuperAdmin
 router.post(
-    '/superAdmin/nuevo/empleado',
+    '/superAdmin/nuevo/proveedor',
     authMiddleware,
     roleMiddleware('SuperAdmin'),
     validateMiddleware(supplierSchema),
     addSupplier
 );
+router.get('/superAdmin/ver/proveedores', authMiddleware, roleMiddleware('SuperAdmin'), readSuppliers);
 
 //Configuración de rutas (endpoints) para el Admin
 router.post(
-    '/admin/nuevo/empleado',
+    '/admin/nuevo/proveedor',
     authMiddleware,
     roleMiddleware('Admin'),
     validateMiddleware(supplierSchema),
     addSupplier
 );
+router.get('/admin/ver/proveedores', authMiddleware, roleMiddleware('Admin'), readSuppliers);
 
 //Exportación de todas las rutas de empleado
 module.exports = router;
