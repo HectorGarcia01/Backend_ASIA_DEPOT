76a93f87d1082a3b7333fbfe65e6f5f9cc97b35d
HectorGarcia01 <hg2779948@gmail.com>
2023-08-26 03:57:10 -0600

    Se agregó la ruta (endpoint) del producto en la instancia de express

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const authRoutes = require('./routes/auth.routes');
 const imagesRoutes = require('./routes/upload_images.routes');
 const supplierRoutes = require('./routes/supplier.routes');
 const categoryRoutes = require('./routes/category.routes');
+const productRoutes = require('./routes/product.routes');
 
 const app = express();    
 
@@ -40,6 +41,7 @@ app.use(authRoutes);
 app.use(imagesRoutes);
 app.use(supplierRoutes);
 app.use(categoryRoutes);
+app.use(productRoutes);
 
 //Configuración del manejo de rutas inexistentes
 app.get('*', (req, res) => {
