304babad6e4a4f926ec424a43a18d088718e795f
HectorGarcia01 <hg2779948@gmail.com>
2023-08-26 03:06:17 -0600

    Configuración de las rutas (endpoints) para obtener las categorias de la bd

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = new express.Router();
 const {
-    addCategory
+    addCategory,
+    readCategories
 } = require('../controllers/category.controller');
 const {
     categorySchema
@@ -12,11 +13,14 @@ const roleMiddleware = require('../middlewares/check_rol');
 
 //Rutas (endpoints) para el SuperAdmin
 router.post('/superAdmin/crear/categoria', authMiddleware, roleMiddleware('SuperAdmin'), validate(categorySchema), addCategory);
+router.get('/auperAdmin/ver/categorias', authMiddleware, roleMiddleware('SuperAdmin'), readCategories);
 
 //Rutas (endpoints) para el Admin
 router.post('/admin/crear/categoria', authMiddleware, roleMiddleware('Admin'), validate(categorySchema), addCategory);
+router.get('/admin/ver/categorias', authMiddleware, roleMiddleware('Admin'), readCategories);
 
 //Rutas (endpoints) para el User
+router.get('/usuario/ver/categorias', readCategories);
 
 //Exportación de todas las rutas de categoría
 module.exports = router;
