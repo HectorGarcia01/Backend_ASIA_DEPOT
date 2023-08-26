8eee3fc0f0466c3468068bcfa77096f1f86d4fcb
HectorGarcia01 <hg2779948@gmail.com>
2023-08-26 04:34:51 -0600

    Configuración de las rutas (endpoints) para que el SuperAdmin y el Admin puedan eliminar un producto de forma lógica por su id

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,7 +4,8 @@ const {
     addProduct,
     readProducts,
     readProductId,
-    updateProduct
+    updateProductId,
+    deleteProductId
 } = require('../controllers/product.controller');
 const {
     productSchema
@@ -17,13 +18,15 @@ const roleMiddleware = require('../middlewares/check_rol');
 router.post('/superAdmin/crear/producto', authMiddleware, roleMiddleware('SuperAdmin'), validate(productSchema), addProduct);
 router.get('/superAdmin/ver/productos', authMiddleware, roleMiddleware('SuperAdmin'), readProducts);
 router.get('/superAdmin/ver/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), readProductId);
-router.patch('/superAdmin/actualizar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), updateProduct);
+router.patch('/superAdmin/actualizar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), updateProductId);
+router.delete('/superAdmin/eliminar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteProductId);
 
 //Rutas (endpoints) para el Admin
 router.post('/admin/crear/producto', authMiddleware, roleMiddleware('Admin'), validate(productSchema), addProduct);
 router.get('/admin/ver/productos', authMiddleware, roleMiddleware('Admin'), readProducts);
 router.get('/admin/ver/producto/:id', authMiddleware, roleMiddleware('Admin'), readProductId);
-router.get('/admin/actualizar/producto/:id', authMiddleware, roleMiddleware('Admin'), updateProduct);
+router.get('/admin/actualizar/producto/:id', authMiddleware, roleMiddleware('Admin'), updateProductId);
+router.delete('/admin/eliminar/producto/:id', authMiddleware, roleMiddleware('Admin'), deleteProductId);
 
 //Rutas (endpoints) para el User
 router.get('/usuario/ver/productos', readProducts);
