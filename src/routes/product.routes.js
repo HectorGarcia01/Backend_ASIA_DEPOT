fbde251b3ec36fbdb8aea8b3ce9d50bac31d6718
HectorGarcia01 <hg2779948@gmail.com>
2023-08-26 04:28:49 -0600

    Configuración de las rutas (endpoints) para que el SuperAdmin y el Admin actualicen los datos de un producto por su id

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,7 +3,8 @@ const router = new express.Router();
 const {
     addProduct,
     readProducts,
-    readProductId
+    readProductId,
+    updateProduct
 } = require('../controllers/product.controller');
 const {
     productSchema
@@ -16,11 +17,13 @@ const roleMiddleware = require('../middlewares/check_rol');
 router.post('/superAdmin/crear/producto', authMiddleware, roleMiddleware('SuperAdmin'), validate(productSchema), addProduct);
 router.get('/superAdmin/ver/productos', authMiddleware, roleMiddleware('SuperAdmin'), readProducts);
 router.get('/superAdmin/ver/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), readProductId);
+router.patch('/superAdmin/actualizar/producto/:id', authMiddleware, roleMiddleware('SuperAdmin'), updateProduct);
 
 //Rutas (endpoints) para el Admin
 router.post('/admin/crear/producto', authMiddleware, roleMiddleware('Admin'), validate(productSchema), addProduct);
 router.get('/admin/ver/productos', authMiddleware, roleMiddleware('Admin'), readProducts);
 router.get('/admin/ver/producto/:id', authMiddleware, roleMiddleware('Admin'), readProductId);
+router.get('/admin/actualizar/producto/:id', authMiddleware, roleMiddleware('Admin'), updateProduct);
 
 //Rutas (endpoints) para el User
 router.get('/usuario/ver/productos', readProducts);
