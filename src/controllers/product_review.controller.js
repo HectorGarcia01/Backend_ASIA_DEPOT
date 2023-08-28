9f66f5331793d70bca080fb9fd0a6ef3cbae4101
HectorGarcia01 <hg2779948@gmail.com>
2023-08-28 03:52:25 -0600

    Creación de la función para ver todas las reseñas que ha hecho el cliente autenticado sobre un producto por id

--- src/controllers/product_review.controller.js
+++ src/controllers/product_review.controller.js
@@ -66,8 +66,39 @@ const readProductReviews = async (req, res) => {
     }
 };
 
+/**
+ * Función para ver todas las reseñas que ha hecho el cliente sobre un producto por id
+ * Fecha creación: 28/08/2023
+ * Autor: Hector Armando García González
+ * Referencias:
+ *              Modelo Valoración de Producto (product_review.js), 
+ */
+
+const readCustomerReviews = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { user } = req;
+
+        const customerReviews = await ProductReviewModel.findAll({
+            where: {
+                ID_Cliente_FK: user.id,
+                ID_Producto_FK: id
+            }
+        });
+
+        if (customerReviews.length === 0) {
+            return res.status(404).send({ error: "No has hecho ninguna reseña." });
+        }
+
+        res.status(200).send({ customerReviews });
+    } catch (error) {
+        res.status(500).send({ error: "Error interno del servidor." });
+    }
+};
+
 //Exportación de controladores para la reseña del producto
 module.exports = {
     addProductReview,
-    readProductReviews
+    readProductReviews,
+    readCustomerReviews
 };
