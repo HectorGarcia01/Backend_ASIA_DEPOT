d1d176d0fb1f392d6d3dbda379f6be3662dc9a4a
HectorGarcia01 <hg2779948@gmail.com>
2023-08-28 02:30:14 -0600

    Creación del esquema de validación para la valoración de produtos

---
+++ src/schemas/product_review.schema.js
@@ -0,0 +1,35 @@
+const Joi = require('joi');
+const customError = require('../utils/custom_error');
+
+/**
+ * Esquema de validación de valoración de producto
+ * Fecha creación: 28/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              custom_error.js (para errores personalizados)
+ */
+
+const productReviewValidateSchema = Joi.object({
+    Comentario_Producto: Joi.string()
+        .trim(),
+    Puntuacion_Producto: Joi.number()
+        .integer()
+        .error((error) => {
+            return customError("La puntuación es obligatoria y numérica.", error);
+        }),
+    ID_Cliente_FK: Joi.number()
+        .integer()
+        .required()
+        .error((error) => {
+            return customError("El ID del cliente es obligatorio y numérico.", error);
+        }),
+    ID_Producto_FK: Joi.number()
+        .integer()
+        .required()
+        .error((error) => {
+            return customError("El ID del producto es obligatorio y numérico.", error);
+        }),
+});
+
+//Exportación del esquema de validación para la valoración de producto
+module.exports = productReviewValidateSchema;
