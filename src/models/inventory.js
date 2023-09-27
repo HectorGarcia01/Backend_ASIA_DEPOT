4e3fe822b71def815c41df6d1c7d66544b979d48
HectorGarcia01 <hg2779948@gmail.com>
2023-09-26 18:11:28 -0600

    Configuración de la relación de uno a muchos de empleado hacia inventario y también de la relación de uno a muchos entre producto e inventario

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -1,5 +1,7 @@
 const { DataTypes } = require('sequelize');
 const db = require('../database/db_connection');
+const Empleado = require('../models/employee');
+const Producto = require('../models/product');
 
 /**
  * Creación del modelo Inventario
@@ -36,6 +38,44 @@ const Inventario = db.define('PRGADH_Inventario', {
             key: 'id'
         }
     }
+});
+
+/**
+ * Configurando la relación de uno a muchos
+ * Fecha creación: 26/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Empleado (employee.js) -> uno
+ *              Modelo Inventario (inventory.js)  -> muchos
+ */
+
+Empleado.hasMany(Inventario, {
+    foreignKey: 'ID_Departamento_FK',
+    as: 'inventarios'
+});
+
+Inventario.belongsTo(Empleado, {
+    foreignKey: 'ID_Departamento_FK',
+    as: 'empleado'
+});
+
+/**
+ * Configurando la relación de uno a muchos
+ * Fecha creación: 26/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Producto (product.js) -> uno
+ *              Modelo Inventario (inventory.js)  -> muchos
+ */
+
+Producto.hasMany(Inventario, {
+    foreignKey: 'ID_Departamento_FK',
+    as: 'inventarios'
+});
+
+Inventario.belongsTo(Producto, {
+    foreignKey: 'ID_Departamento_FK',
+    as: 'producto'
 });
 
 //Exportación del modelo Inventario
