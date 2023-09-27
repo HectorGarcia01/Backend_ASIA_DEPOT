138ec17035a712ab50ec14c11f2c316a1e4e89f9
HectorGarcia01 <hg2779948@gmail.com>
2023-09-27 05:24:16 -0600

    Modificación de la función para validar la existencia de municipio

--- src/utils/customer/validate_municipality.js
+++ src/utils/customer/validate_municipality.js
@@ -1,13 +1,14 @@
-const { handleSuccess, handleResponse } = require('../response_handling');
 const MunicipalityModel = require('../../models/municipality');
 
 /**
  * Función para para validar si el municipio ingresado existe
  * Fecha creación: 27/09/2023
  * Autor: Hector Armando García González
+ * Referencias:
+ *              Modelo Municipio (municipality.js)
  */
 
-const validateMunicipality = async (res, ID_Municipio_FK, ID_Departamento_FK) => {
+const validateMunicipality = async (ID_Municipio_FK, ID_Departamento_FK) => {
     const addressCustomer = await MunicipalityModel.findOne({
         where: {
             id: ID_Municipio_FK,
@@ -16,7 +17,9 @@ const validateMunicipality = async (res, ID_Municipio_FK, ID_Departamento_FK) =>
     });
 
     if (!addressCustomer) {
-        handleResponse(res, 404, "Municipio no encontrado.");
+        const error = new Error("Municipio no encontrado.");
+        error.status = 404;
+        throw error;
     }
 };
 
