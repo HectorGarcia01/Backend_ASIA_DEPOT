73aa299e0be3bdb392e44f54f0c292bfe9b9c9b4
HectorGarcia01 <hg2779948@gmail.com>
2023-09-26 19:31:03 -0600

    Configuración de las relaciones de uno a muchos entre empleado y token, también entre cliente y token

--- src/models/token.js
+++ src/models/token.js
@@ -1,5 +1,7 @@
 const { DataTypes } = require('sequelize');
 const db = require('../database/db_connection');
+const Cliente = require('../models/customer');
+const Empleado = require('../models/employee');
 
 /**
  * Creación del modelo Token
@@ -29,6 +31,44 @@ const Token = db.define('PRGADH_Token', {
             key: 'id'
         }
     }
+});
+
+/**
+ * Configurando la relación de uno a muchos
+ * Fecha creación: 26/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Cliente (customer.js) -> uno
+ *              Modelo Token (token.js)  -> muchos
+ */
+
+Cliente.hasMany(Token, {
+    foreignKey: 'ID_Cliente_FK',
+    as: 'tokens'
+});
+
+Token.belongsTo(Cliente, {
+    foreignKey: 'ID_Cliente_FK',
+    as: 'cliente'
+});
+
+/**
+ * Configurando la relación de uno a muchos
+ * Fecha creación: 26/09/2023
+ * Autor: Hector Armando García González
+ * Referencia:
+ *              Modelo Empleado (employee.js) -> uno
+ *              Modelo Token (token.js)  -> muchos
+ */
+
+Empleado.hasMany(Token, {
+    foreignKey: 'ID_Empleado_FK',
+    as: 'tokens'
+});
+
+Token.belongsTo(Empleado, {
+    foreignKey: 'ID_Empleado_FK',
+    as: 'empleado'
 });
 
 //Exportación del modelo Token
