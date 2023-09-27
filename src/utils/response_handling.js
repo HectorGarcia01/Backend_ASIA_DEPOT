cd5c1933eadba6647f9e7e3892709ddd273b1a67
HectorGarcia01 <hg2779948@gmail.com>
2023-09-27 04:09:28 -0600

    Creación de la función para el manejo de respuesta y estados de error, además de la función para el manejo de respuesta y estados de éxito

---
+++ src/utils/response_handling.js
@@ -0,0 +1,25 @@
+/**
+ * Función para el manejo de respuestas con estados existosos
+ * Fecha creación: 27/09/2023
+ * Autor: Hector Armando García González
+ */
+
+const handleSuccess = (res, status = 200, msg = null) => {
+    res.status(status).send({ msg });
+};
+
+/**
+ * Función para el manejo de respuestas con estados de error
+ * Fecha creación: 27/09/2023
+ * Autor: Hector Armando García González
+ */
+
+const handleResponse = (res, status = 500, error = "Error interno del servidor.") => {
+    res.status(status).send({ error });
+};
+
+//Exportación de las funciones para el manejo de respuestas y estados
+module.exports = {
+    handleSuccess,
+    handleResponse
+};
