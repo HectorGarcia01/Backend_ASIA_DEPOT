4e6a10cf2dca551827dd985294cfe3fd71238910
HectorGarcia01 <hg2779948@gmail.com>
2023-10-02 15:40:47 -0600

    Se implementó el esquema de validación de datos de actualización

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -8,7 +8,8 @@ const {
     deleteCategoryId
 } = require('../controllers/category.controller');
 const {
-    categorySchema
+    categorySchema,
+    updateCategorySchema
 } = require('../schemas/category.schema');
 const validate = require('../middlewares/validate');
 const authMiddleware = require('../middlewares/auth');
@@ -18,14 +19,14 @@ const roleMiddleware = require('../middlewares/check_rol');
 router.post('/superAdmin/crear/categoria', authMiddleware, roleMiddleware('SuperAdmin'), validate(categorySchema), addCategory);
 router.get('/superAdmin/ver/categorias', authMiddleware, roleMiddleware('SuperAdmin'), readCategories);
 router.get('/superAdmin/ver/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), readCategoryId);
-router.patch('/superAdmin/actualizar/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), updateCategoryId);
+router.patch('/superAdmin/actualizar/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), validate(updateCategorySchema), updateCategoryId);
 router.delete('/superAdmin/eliminar/categoria/:id', authMiddleware, roleMiddleware('SuperAdmin'), deleteCategoryId);
 
 //Rutas (endpoints) para el Admin
 router.post('/admin/crear/categoria', authMiddleware, roleMiddleware('Admin'), validate(categorySchema), addCategory);
 router.get('/admin/ver/categorias', authMiddleware, roleMiddleware('Admin'), readCategories);
 router.get('/admin/ver/categoria/:id', authMiddleware, roleMiddleware('Admin'), readCategoryId);
-router.patch('/admin/actualizar/categoria/:id', authMiddleware, roleMiddleware('Admin'), updateCategoryId);
+router.patch('/admin/actualizar/categoria/:id', authMiddleware, roleMiddleware('Admin'), validate(updateCategorySchema), updateCategoryId);
 
 //Rutas (endpoints) para el User
 router.get('/usuario/ver/categorias', readCategories);
