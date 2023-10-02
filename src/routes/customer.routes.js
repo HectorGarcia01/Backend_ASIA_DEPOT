7b3cfc6248c11e14bc89e6a5b7fe7c12abad183d
HectorGarcia01 <hg2779948@gmail.com>
2023-10-02 15:27:49 -0600

    Se implementó el esquema de validación para la actualización de los datos del cliente

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -5,14 +5,23 @@ const {
     customerProfile,
     updateCustomer
 } = require('../controllers/customer.controller');
-const customerSchema = require('../schemas/customer.schema');
+const {
+    customerValidateSchema,
+    updateCustomerValidateSchema
+} = require('../schemas/customer.schema');
 const validateMiddleware = require('../middlewares/validate');
 const authMiddleware = require('../middlewares/auth');
 const roleMiddleware = require('../middlewares/check_rol');
 
-router.post('/nuevo/cliente', validateMiddleware(customerSchema), addCustomer);
+router.post('/nuevo/cliente', validateMiddleware(customerValidateSchema), addCustomer);
 router.get('/usuario/ver/perfil', authMiddleware, roleMiddleware('User'), customerProfile);
-router.patch('/usuario/actualizar/perfil', authMiddleware, roleMiddleware('User'), updateCustomer);
+router.patch(
+    '/usuario/actualizar/perfil', 
+    authMiddleware, 
+    roleMiddleware('User'), 
+    validateMiddleware(updateCustomerValidateSchema), 
+    updateCustomer
+);
 
 //Exportación de todas las rutas de cliente
 module.exports = router;
