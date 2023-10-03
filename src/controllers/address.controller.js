5dae0b4b8238cbc010cf7eedc86ce42675e88b8d
HectorGarcia01 <hg2779948@gmail.com>
2023-10-03 15:50:24 -0600

    Creación de la función para crear un nuevo municipio

--- src/controllers/address.controller.js
+++ src/controllers/address.controller.js
@@ -1,22 +1,58 @@
 const Sequelize = require('sequelize');
+const {
+    findDepartment,
+    findMunicipality
+} = require('../utils/find_address');
 const DepartmentModel = require('../models/department');
 const MunicipalityModel = require('../models/municipality');
 
 /**
- * Función para registrar una nueva dirección (departamento y municipio)
+ * Función para registrar un nuevo departamento
  * Fecha creación: 26/09/2023
  * Autor: Hector Armando García González
  * Referencias: 
  *              Modelo Departamento (department.js), 
- *              Modelo Municipio (municipality.js)
  */
 
-const addAddress = (req, res) => {
+const addDepartment = async (req, res) => {
     try {
+        const { Nombre_Departamento } = req.body;
 
+        const newDepartment = await DepartmentModel.create({ Nombre_Departamento });
+
+        res.status(201).send({ msg: "Se ha registrado un nuevo departamento.", newDepartment })
     } catch (error) {
         if (error instanceof Sequelize.UniqueConstraintError) {
             res.status(400).send({ error: "¡El departamento ya existe!" });
+        } else {
+            res.status(500).send({ error: "Error interno del servidor." });
+        }
+    }
+};
+
+/**
+ * Función para registrar un nuevo municipio
+ * Fecha creación: 26/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Municipio (municipality.js), 
+ */
+
+const addMunicipality = async (req, res) => {
+    try {
+        const { Nombre_Municipio, ID_Departamento_FK } = req.body;
+
+        await findDepartment(ID_Departamento_FK);
+
+        const newMunicipality = await MunicipalityModel.create({ 
+            Nombre_Municipio,
+            ID_Departamento_FK
+        });
+
+        res.status(201).send({ msg: "Se ha registrado un nuevo municipio.", newMunicipality })
+    } catch (error) {
+        if (error instanceof Sequelize.UniqueConstraintError) {
+            res.status(400).send({ error: "¡El muni ya existe!" });
         } else {
             res.status(500).send({ error: "Error interno del servidor." });
         }
@@ -52,6 +88,7 @@ const readAddresses = async (req, res) => {
 };
 
 module.exports = {
-    addAddress,
+    addDepartment,
+    addMunicipality,
     readAddresses
 };
