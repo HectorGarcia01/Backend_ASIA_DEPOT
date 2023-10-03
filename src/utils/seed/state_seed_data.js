a6a9ec50d6f66a422bda31ece69378269699db27
HectorGarcia01 <hg2779948@gmail.com>
2023-10-02 22:43:18 -0600

    Se agrega el nuevo estado a la siembra de datos

--- src/utils/seed/state_seed_data.js
+++ src/utils/seed/state_seed_data.js
@@ -3,6 +3,7 @@ module.exports = [
     { Tipo_Estado: 'Pendiente' },
     { Tipo_Estado: 'Activo' },
     { Tipo_Estado: 'Inactivo' },
+    { Tipo_Estado: 'Carrito' },
     { Tipo_Estado: 'En proceso' },
     { Tipo_Estado: 'Completado' }
 ]
