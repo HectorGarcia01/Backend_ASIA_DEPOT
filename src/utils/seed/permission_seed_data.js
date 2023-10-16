498d3714099f32629f66425ceba8775983852bc4
HectorGarcia01 <hg2779948@gmail.com>
2023-10-15 22:38:15 -0600

    Creación de datos de siembra para los permisos

---
+++ src/utils/seed/permission_seed_data.js
@@ -0,0 +1,19 @@
+//Exportación de nombre y descripción de permisos
+module.exports = [
+    {
+        Nombre_Permiso: 'Crear',
+        Descripcion_Permiso: "Le permite al admin/superAdmin la creación de nuevos registros."
+    },
+    {
+        Nombre_Permiso: 'Ver',
+        Descripcion_Permiso: "Le permite al admin/superAdmin ver uno o varios registros."
+    },
+    {
+        Nombre_Permiso: 'Modificar',
+        Descripcion_Permiso: "Le permite al admin/superAdmin modificar un registro en específico."
+    },
+    {
+        Nombre_Permiso: 'Eliminar',
+        Descripcion_Permiso: "Le permite únicamente al superAdmin la eliminación de registros."
+    }
+]
