c01a959e7eed352f15d25068fe20c4952a0b41c5
HectorGarcia01 <hg2779948@gmail.com>
2023-10-16 03:31:16 -0600

    Se importó la constante que posee el prefijo aleatorio y se exportó

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: `.dev.env` });
+require('dotenv').config({ path: `.local.env` });
+const randomPrefix = require('../utils/generate_prefix');
 
 //Exportando las variables de entorno
 module.exports = {
@@ -7,5 +8,6 @@ module.exports = {
     USERNAME_DB: process.env.USERNAME_DB,
     PASSWORD_DB: process.env.PASSWORD_DB,
     HOST_DB: process.env.HOST_DB,
-    KEY_TOKEN: process.env.KEY_TOKEN
+    KEY_TOKEN: process.env.KEY_TOKEN,
+    NAME_PREFIX: randomPrefix
 };
