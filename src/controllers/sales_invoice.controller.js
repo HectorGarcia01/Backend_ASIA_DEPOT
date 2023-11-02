ea077516d47487a2960d9b5a0673ef5a9fd96162
HectorGarcia01 <hg2779948@gmail.com>
2023-11-02 01:16:55 -0600

    Creación de la función para ver todas las ventas, para ver una venta por su id, para cambiar el estado en proceso y para completar una venta

---
+++ src/controllers/sales_invoice.controller.js
@@ -0,0 +1,230 @@
+const SalesInvoiceModel = require('../models/sales_invoice');
+const SalesDetailModel = require('../models/sales_detail');
+const ProductModel = require('../models/product');
+const EmployeeModel = require('../models/employee');
+const InventoryModel = require('../models/inventory');
+const StateModel = require('../models/state');
+
+/**
+ * Función para ver todas las ventas
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Factura_Venta (sales_invoice.js), 
+ *              Modelo Empleado (employee.js),
+ *              Modelo Estado (state.js),
+ *              Función para buscar estado (find_state.js)
+ */
+
+const readSalesInvoices = async (req, res) => {
+    try {
+        const { query } = req;
+        const where = {};
+
+        if (query.estado) {
+            const stateSalesInvoice = await findState(query.estado);
+            where.ID_Estado_FK = stateSalesInvoice.id
+        }
+
+        const salesInvoice = await SalesInvoiceModel.findAll({
+            where,
+            attributes: ['id', 'Numero_Orden', 'Total_Factura'],
+            include: [{
+                model: EmployeeModel,
+                as: 'empleado',
+                attributes: ['id', 'Nombre_Empleado', 'Apellido_Empleado']
+            }, {
+                model: StateModel,
+                as: 'estado',
+                attributes: ['id', 'Tipo_Estado']
+            }]
+        });
+
+        if (salesInvoice.length === 0) {
+            return res.status(404).send({ error: "No hay ninguna venta registrada." });
+        }
+
+        res.status(200).send({ salesInvoice });
+    } catch (error) {
+        if (error.status === 404) {
+            res.status(error.status).send({ error: error.message });
+        } else {
+            res.status(500).send({ error: "Error interno del servidor." });
+        }
+    }
+};
+
+/**
+ * Función para ver el detalle de venta por id
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Factura_Venta (sales_invoice.js), 
+ *              Modelo Detalle_Venta (sales_detail.js),
+ *              Modelo Producto (product.js)
+ *              Modelo Estado (state.js)
+ */
+
+const readSalesInvoiceId = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const salesInvoice = await SalesInvoiceModel.findByPk(id, {
+            attributes: ['id', 'Total_Factura'],
+            include: [{
+                model: SalesDetailModel,
+                as: 'detalles_venta',
+                attributes: ['id', 'Cantidad_Producto', 'Precio_Unitario', 'Subtotal_Venta'],
+                include: {
+                    model: ProductModel,
+                    as: 'producto',
+                    attributes: ['id', 'Nombre_Producto', 'Descripcion_Producto']
+                }
+            },
+            {
+                model: StateModel,
+                as: 'estado',
+                attributes: ['id', 'Tipo_Estado']
+            }]
+        });
+
+        if (!salesInvoice) {
+            return res.status(404).send({ error: "Detalle de venta no encontrado." });
+        }
+
+        res.status(200).send({ salesInvoice });
+    } catch (error) {
+        res.status(500).send({ error: "Error interno del servidor." });
+    }
+};
+
+/**
+ * Función para cambiar estado de la venta (pendiente -> en proceso)
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Factura_Venta (sales_invoice.js),
+ *              Función para buscar estado (find_state.js)
+ */
+
+const changeSalesInvoiceProcess = async (req, res) => {
+    try {
+        const { user } = req;
+        const { id } = req.params;
+
+        const stateSalesInvoice = await findState('Pendiente');
+        const salesInvoice = await SalesInvoiceModel.findOne({
+            where: {
+                id,
+                ID_Estado_FK: stateSalesInvoice.id
+            }
+        });
+
+        if (!salesInvoice) {
+            return res.status(404).send({ error: "Venta no encontrada." });
+        }
+
+        const changeSalesStatus = await findState("En proceso");
+
+        salesInvoice.ID_Estado_FK = changeSalesStatus.id;
+        salesInvoice.ID_Empleado_FK = user.id;
+        await salesInvoice.save();
+
+        //*************Se podría implementar una lógica para enviar el correo de que ya fue completado su pedido */
+        res.status(200).send({ msg: "Venta en proceso." });
+    } catch (error) {
+        if (error.status === 404) {
+            res.status(error.status).send({ error: error.message });
+        } else {
+            res.status(500).send({ error: "Error interno del servidor." });
+        }
+    }
+};
+
+/**
+ * Función para cambiar estado de la venta y generar inventario (en proceso -> completo)
+ * Fecha creación: 29/09/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Modelo Factura_Venta (sales_invoice.js), 
+ *              Modelo Detalle_Venta (sales_detail.js),
+ *              Modelo Producto (product.js),
+ *              Modelo Estado (state.js),
+ *              Modelo Inventario (inventory.js),
+ *              Función para buscar estado (find_state.js)
+ */
+
+const changeSalesInvoiceComplete = async (req, res) => {
+    try {
+        const { user } = req;
+        const { id } = req.params;
+
+        const stateSalesInvoice = await findState("En proceso");
+        const salesInvoice = await SalesInvoiceModel.findOne({
+            where: {
+                id,
+                ID_Empleado_FK: user.id,
+                ID_Estado_FK: stateSalesInvoice.id
+            },
+            attributes: ['id', 'Total_Factura'],
+            include: [{
+                model: SalesDetailModel,
+                as: 'detalles_venta',
+                attributes: ['id', 'Cantidad_Producto', 'Precio_Unitario', 'Subtotal_Venta'],
+                include: {
+                    model: ProductModel,
+                    as: 'producto',
+                    attributes: ['id', 'Nombre_Producto', 'Descripcion_Producto']
+                }
+            },
+            {
+                model: StateModel,
+                as: 'estado',
+                attributes: ['id', 'Tipo_Estado']
+            }]
+        });
+
+        if (!salesInvoice) {
+            return res.status(404).send({ error: "Venta no encontrada." });
+        }
+
+        const changeSalesStatus = await findState('Completado');
+
+        for (const salesDetail of salesInvoice.detalles_venta) {
+            const product = await ProductModel.findByPk(salesDetail.producto.id);
+
+            if (product) {
+                const inventoryEntry = await InventoryModel.create({
+                    tipo_movimiento: 'Venta',
+                    cantidad_movimiento: salesDetail.Cantidad_Producto,
+                    monto_movimiento: salesInvoice.Total_Factura,
+                    ID_Empleado_FK: user.id,
+                    ID_Producto_FK: product.id,
+                });
+
+                if (!inventoryEntry) {
+                    return res.status(500).send({ error: "Error al crear el registro de inventario." });
+                }
+            }
+        }
+
+        salesInvoice.ID_Estado_FK = changeSalesStatus.id;
+        await salesInvoice.save();
+
+        //*************Se podría implementar una lógica para enviar el correo de que ya fue completado su pedido */
+        res.status(200).send({ msg: "Venta completada." });
+    } catch (error) {
+        if (error.status === 404) {
+            res.status(error.status).send({ error: error.message });
+        } else {
+            res.status(500).send({ error: "Error interno del servidor." });
+        }
+    }
+};
+
+module.exports = {
+    readSalesInvoices,
+    readSalesInvoiceId,
+    changeSalesInvoiceProcess,
+    changeSalesInvoiceComplete
+}
