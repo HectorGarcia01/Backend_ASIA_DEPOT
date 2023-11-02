b828b40e75fc89bf278f3f83a9879a916838aa2a
HectorGarcia01 <hg2779948@gmail.com>
2023-11-02 14:43:59 -0600

    Creación de la función para enviar un correo de anulación de suscripción al newsletter

--- src/email/controllers/newsletter.js
+++ src/email/controllers/newsletter.js
@@ -28,6 +28,33 @@ const newsletterEmail = async (destination) => {
     }
 };
 
+/**
+ * Función para enviar un correo de anulación de suscripción al newsletter
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Transportador (email.js)
+ */
+
+const unsubscriptionNewsletterEmail = async (destination) => {
+    try {
+        const pathHTML = path.join(__dirname, '../views/unsubscription_newsletter.ejs');
+        const html = fs.readFileSync(pathHTML, 'utf8');
+
+        const mailOptions = {
+            from: "ASIA DEPOT",
+            to: destination,
+            subject: "Anulación de suscripción al newsletter.",
+            html
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
 module.exports = {
-    newsletterEmail
+    newsletterEmail,
+    unsubscriptionNewsletterEmail
 };
