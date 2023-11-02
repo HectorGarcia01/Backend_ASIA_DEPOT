c806dd594d672832dc9df6cb426f35bd09f6c4cc
HectorGarcia01 <hg2779948@gmail.com>
2023-11-01 22:46:24 -0600

    Creación de la función para enviar un correo de suscipción al newsletter

---
+++ src/email/controllers/newsletter.js
@@ -0,0 +1,33 @@
+const transporter = require('./email');
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Función para enviar un correo de suscripción al newsletter
+ * Fecha creación: 22/08/2023
+ * Autor: Hector Armando García González
+ * Referencias: 
+ *              Transportador (email.js)
+ */
+
+const newsletterEmail = async (destination) => {
+    try {
+        const pathHTML = path.join(__dirname, 'views/newsletter.ejs');
+        const html = fs.readFileSync(pathHTML, 'utf8');
+
+        const mailOptions = {
+            from: "ASIA DEPOT",
+            to: destination,
+            subject: "Suscripción al newsletter.",
+            html
+        };
+
+        const info = await transporter.sendMail(mailOptions);
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+module.exports = {
+    newsletterEmail
+};
