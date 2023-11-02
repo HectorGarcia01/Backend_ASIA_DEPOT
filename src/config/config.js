f55dbceaa7bdc18b258001792ea9789f2c683262
HectorGarcia01 <hg2779948@gmail.com>
2023-11-01 21:44:15 -0600

    Se agregaron las variables de entorno para el manejo del correo electrónico

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: `.env.env` });
+require('dotenv').config({ path: `.dev.env` });
 const randomPrefix = require('../utils/generate_prefix');
 
 //Exportando las variables de entorno
